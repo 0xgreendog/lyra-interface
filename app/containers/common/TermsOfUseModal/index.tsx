3c7dd16ed116d8486348efd1274f2bd0adaf4886
greendog <imgreendog@proton.me>
2022-10-07 09:35:28 -0600

    type

--- app/containers/common/TermsOfUseModal/index.tsx
+++ app/containers/common/TermsOfUseModal/index.tsx
@@ -44,7 +44,7 @@ export default function TermsOfUseModal({ isOpen, onClose, onConfirm }: Props) {
           </Box>
           <Text color="secondaryText">
             In case of an investigation by any federal entity or similar, I do not have any involvement with this group
-            or with the people in it, I do not know how I am here, probably added by a thrid party, I do not support any
+            or with the people in it, I do not know how I am here, probably added by a third party, I do not support any
             actions by the member of this group.
           </Text>
         </Flex>
